5eabb1f9f172ea1b118aa4c152130d16d85af758
David Araújo <contact@araujodavid.com>
2022-06-28 16:52:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+from __future__ import print_function
+
+import os.path
+import json
+from datetime import datetime
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If you change scope or credentials the token.json file must be deleted before running the script
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
+# Replace with the ID of the folder that will be the root for the search for the files
+ROOT_FOLDER_TO_SEARCH = '<ID>'
 
 MAX_PARENTS = 500
 
@@ -88,3 +105,61 @@ def retrieve_files_in_folders(selected_folders):
         filter = ' in parents or '.join('"{0}"'.format(f) for f in folder_list) + ' in parents'
         files.update(get_files_in_folders(filter))
     return files
+
+
+if __name__ == "__main__":
+    creds = None
+
+    """
+    The file token.json stores the user's access and refresh tokens, and is created 
+    automatically when the authorization flow completes for the first time.
+    """
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        api_ref = build('drive', 'v3', credentials=creds)
+
+        print(f'Querying all folders: {datetime.now()}')
+        
+        all_folders_dict = retrieve_all_folders()
+
+        a_file = open("folders.json", "w", encoding='utf-8')
+        json.dump(all_folders_dict, a_file, ensure_ascii=False)
+
+        print(f'Total of folders: {len(all_folders_dict)}')
+
+        print(f'Retrieving folders of interest: {datetime.now()}')
+
+        relevant_folders_list = [ROOT_FOLDER_TO_SEARCH]
+
+        for folder in retrieve_subfolders(ROOT_FOLDER_TO_SEARCH, all_folders_dict):
+           relevant_folders_list.append(folder)
+
+        print(f'Total of folders of interest: {len(relevant_folders_list)}')
+
+        print(f'Retrieving files from selected folders: {datetime.now()}')
+
+        all_files_dict = retrieve_files_in_folders(relevant_folders_list)
+
+        print(f'Total of files: {len(all_files_dict)}')
+
+        b_file = open("files.json", "w", encoding='utf-8')
+        json.dump(all_files_dict, b_file, ensure_ascii=False)
+
+        print(f'Finishing the job: {datetime.now()}')
+
+    except HttpError as error:
+        print(f'An error occurred: {error}')
